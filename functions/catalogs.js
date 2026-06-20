314fffe4d1df5060adf85fb57c28025bdddf6d8c
valordev <133082589+blockychara@users.noreply.github.com>
2026-06-20 17:27:41 -0500

    Add onRequestGet function to fetch public catalogs

--- functions/catalogs.js
+++ functions/catalogs.js
@@ -1,1 +1,23 @@
+export async function onRequestGet(
+    context
+){
 
+    const { env } = context;
+
+    const rows =
+        await env.DB.prepare(`
+            SELECT
+                username,
+                catalog_name,
+                description
+            FROM users
+            WHERE is_public=1
+            ORDER BY catalog_name
+        `)
+        .all();
+
+    return Response.json(
+        rows.results
+    );
+
+}
