40e1ba6e82d2b1306df99f82f1aff83ca59a0db6
valordev <133082589+blockychara@users.noreply.github.com>
2026-06-20 17:27:20 -0500

    Add logout function to clear session cookie
    Implement logout functionality by clearing session cookie.

--- functions/logout.js
+++ functions/logout.js
@@ -1,1 +1,15 @@
+export async function onRequestPost(
+    context
+){
 
+    return new Response(
+        "",
+        {
+            headers:{
+                "Set-Cookie":
+                "session=; Path=/; Max-Age=0"
+            }
+        }
+    );
+
+}
